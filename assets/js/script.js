9e87c8680c2a013a53dfd8c1d29cf9c1ae6f4033
Mtan1230 <m.tan1230@gmail.com>
2023-04-27 01:56:26 -0400

    add event-listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,20 @@ const nextBtn = document.querySelectorAll(".next-btn");
 const showAnswer = document.getElementById("answer");
 const result = document.getElementById("result");
 const highScore = localStorage.getItem("score");
+const returnBtn = getElementById("return-btn");
+const clearBtn = getElementById("clear-btn");
 
 let secLeft = 75;
 let score = 0;
 let cardIndex = 0;
 
+//event listeners
+nextBtn.forEach(function(btn) {
+    btn.addEventListener("click", next);
+});
+
+returnBtn.addEventListener("click", function() {
+    document.reload();
+})
+
+clearBtn.addEventListener("click", clearScore);
